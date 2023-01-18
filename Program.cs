ed63c7186a1f089310eec671f07503ff3d43ed19
shamilhuseinov <shamilrh@code.edu.az>
2023-01-18 13:09:59 +0400

    homework is added

--- Program.cs
+++ Program.cs
@@ -1,40 +1,95 @@
 ﻿//1
-//int[] numbers = { 100, 300, -200, 50 };
-//int count = 0;
-//for (int i = 0; i < numbers.Length; i++)
-//{
-//    int j = count;
-//    while (j < numbers.Length)
-//    {
-//        if (numbers[j] < numbers[i])
-//        {
-//            int x = numbers[i];
-//            numbers[i] = numbers[j];
-//            numbers[j] = x;
-
-//        }
-//        j++;
-//    }
-//    count++;
-//    Console.WriteLine(numbers[i]);
-//}
+int[,] arr = new int[3, 3]
+{
+    { 3,4,6},
+    { 2,0,7},
+    {3, 5, 7 }
+};
+int sum = 0;
+for (int i = 0; i < arr.GetLength(0); i++)
+{
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+        if (i == 2 - j)
+        {
+            sum += arr[i, j];
+        }
+    }
+}
+Console.WriteLine(sum);
 
 //2
-//int[] numbers = { 100, 300, 250, 50 };
-//int sum = 0;
-//int count = 0;
-//for (int i = 0; i < numbers.Length; i++)
+//int[,] arr = new int[3, 3]
 //{
-//    sum += numbers[i];
-//    count++;
-//}
-//Console.WriteLine($"ədədi orta = {sum / count}");
+//    { 3,4,5},
+//    { 2,0,7},
+//    {3, 5, 7 }
+//};
 
-//3
-//int[] numbers = { 100, 300, -200, 50 };
-//for (int i = numbers.Length - 1; i >= 0; i--)
+//int[,] arr2 = new int[3, 3]
 //{
-//    Console.WriteLine(numbers[i]);
+//    { 4,2,7},
+//    { 1,9,5},
+//    {5, 6, 3 }
+//};
+
+//int[,] arr3 = new int[3, 3]
+//{
+//    { 0,0,0},
+//    { 0,0,0},
+//    {0, 0, 0 }
+//};
+//for (int i = 0; i < arr.GetLength(0); i++)
+//{
+//    for (int j = 0; j < arr.GetLength(1); j++)
+//    {
+//        arr3[i, j] = arr[i, j] + arr2[i, j];
+//        Console.Write(arr3[i, j] + " ");
+//    }
+//    Console.WriteLine();
 //}
 
 
+//3 sol diaqonal
+//int[,] arr = new int[5, 5]
+//{
+//    { 3,4,5,8,1},
+//    { 2,9,7,7,2},
+//    {3, 5, 7 ,0,4},
+//    {4, 5, 4, 3,2 },
+//    {3,2,6,3,7 }
+//};
+//for (int i = 0; i < arr.GetLength(0); i++)
+//{
+//    for (int j = 0; j < arr.GetLength(1); j++)
+//    {
+//        if (i > 4 - j)
+//        {
+//            arr[i, j] = 0;
+//        }
+//        Console.Write(arr[i, j] + " ");
+//    }
+//    Console.WriteLine();
+//}
+
+//11 sag diaqonal
+//int[,] arr = new int[5, 5]
+//{
+//    { 3,4,5,8,1},
+//    { 2,9,7,7,2},
+//    {3, 5, 7 ,0,4},
+//    {4, 5, 4, 3,2 },
+//    {3,2,6,3,7 }
+//};
+//for (int i = 0; i < arr.GetLength(0); i++)
+//{
+//    for (int j = 0; j < arr.GetLength(1); j++)
+//    {
+//        if (i > j)
+//        {
+//            arr[i, j] = 0;
+//        }
+//        Console.Write(arr[i, j] + " ");
+//    }
+//    Console.WriteLine();
+//}
